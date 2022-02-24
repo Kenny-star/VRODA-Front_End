eec05240831bc217b923be2d1659323aba0b6f9b
Kenny <kenny.luoli@hotmail.ca>
2022-02-24 12:36:05 -0500

    po

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
 apiBaseUrl: 'https://vrodapp.herokuapp.com', //Product
 apiBaseUrl2: 'https://vrodapp2.herokuapp.com', //Auth
 apiBaseUrl3: 'https://vrodapp3.herokuapp.com' //Cart
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080/api/gateway'
-};
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:8080/api/gateway'
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
